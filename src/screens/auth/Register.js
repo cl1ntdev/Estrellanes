36c2b4fc4fa42f051d898418ff91f3ad0aa2c556
cl1ntdev <clintjayestrellanes17@gmail.com>
2026-02-20 11:01:59 +0800

    feat: Register functions

--- src/screens/auth/Register.js
+++ src/screens/auth/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Text, View,Button } from 'react-native';
+import {Alert, Text, View,Button, TouchableOpacity } from 'react-native';
 import CustomeTextInput from '../../components/CustomTextInput';
 import { useNavigation } from '@react-navigation/native';
 import { ROUTES } from '../../utils';
@@ -7,7 +7,10 @@ import CustomButton from '../../components/CustomButton'
 
 export default function Register() {
   console.log('this is register')
-
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confPass, setConfPass] = useState('');
+  const navigation = useNavigation();
   return (
     <View
       style={{
@@ -17,7 +20,96 @@ export default function Register() {
         height: '100%',
       }}
     >
-     <Text>Register </Text>
+      <View
+        style={{
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          height: '20%',
+          // borderWidth: 1,
+          width: '100%',
+          paddingHorizontal: 20,
+          marginBottom: 20,
+        }}
+      >
+        <CustomeTextInput
+          label={'Enter Username'}
+          placeholder={'Enter your username'}
+          onChangeText={setUsername}
+          labelStyle={{ color: 'black' }}
+          TextInputStyle={{
+            borderBottomWidth: 1,
+            color: 'black',
+            // borderWidth: 1,
+            // borderColor: 'gray',
+          }}
+          containerStyle={{
+            width: '100%',
+          }}
+        />
+
+        <CustomeTextInput
+          label={'Enter Password'}
+          placeholder={'Enter your Password'}
+          onChangeText={setPassword}
+          labelStyle={{ color: 'black' }}
+          TextInputStyle={{
+            borderBottomWidth: 1,
+            color: 'black',
+            // borderWidth: 1,
+            // borderColor: 'gray',
+          }}
+          containerStyle={{
+            width: '100%',
+          }}
+        />
+        <CustomeTextInput
+          label={'Confirm Password'}
+          placeholder={'Re-enter your Password'}
+          onChangeText={setConfPass}
+          labelStyle={{ color: 'black' }}
+          TextInputStyle={{
+            borderBottomWidth: 1,
+            color: 'black',
+            // borderWidth: 1,
+            // borderColor: 'gray',
+          }}
+          containerStyle={{
+            width: '100%',
+          }}
+        />
+      </View>
+      
+
+      <View
+        style={{
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          width: '100%',
+          paddingHorizontal: 20,
+          marginTop: 20,
+        }}
+      >
+        <CustomButton
+          label={'Register'}
+          containerStyle={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: '#0377FF',
+            padding: 10,
+            borderRadius: 20,
+          }}
+          textStyle={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'white',
+          }}
+          onPress={() => {
+            Alert.alert('Credentials', `u: ${username} p: ${password} `);
+          }}
+        />
+      </View>
+
+      
     </View>
   );
 }
